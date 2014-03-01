4fb929f1ab4693f2cf19ad71a24d8baf39232336
calcdude84se <calcdude84se@gmail.com>
2014-03-01 13:50:37 -0500

    Made main skeleton

--- src/lisp/Main.java
+++ src/lisp/Main.java
@@ -1,14 +1,15 @@
 package lisp;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
+public abstract class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame gameFrame = new JFrame("Lasers In Space! -- and Pong");
+		GameRoom gameRoom = new GameRoom();
+		gameFrame.add(gameRoom.getPanel());
+		gameRoom.startGame();
 	}
 
 }
