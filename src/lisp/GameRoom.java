23409df82bf87388f0d3b49c0eda5415c95d3f28
calcdude84se <calcdude84se@gmail.com>
2014-03-01 16:39:53 -0500

    Record keypresses

--- src/lisp/GameRoom.java
+++ src/lisp/GameRoom.java
@@ -2,9 +2,13 @@ package lisp;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.swing.JPanel;
 
@@ -19,6 +23,8 @@ public class GameRoom {
 	private ScoreBoard scoreBoard;
 	private AsteroidField asteroidField;
 	private Collection<GameObject> gameObjects = new ArrayList<>();
+	private Set<Integer> keysPressed = new HashSet<>();
+	private Set<Integer> keysReleased = new HashSet<>();
 	
 	public GameRoom()
 	{
@@ -50,7 +56,8 @@ public class GameRoom {
 	 * Initializes for one game.
 	 */
 	private void init() {
-		//TODO write method
+		keysPressed.clear();
+		keysReleased.clear();
 		gameObjects.clear();
 		final int shipY = panel.getHeight() / 2;
 		final int shipLeftX = 0, shipLeftY = shipY,
@@ -80,7 +87,42 @@ public class GameRoom {
 		return asteroidField;
 	}
 	
+	private final int shipLeftUp = KeyEvent.VK_Q, shipLeftLaser = KeyEvent.VK_A,
+			shipLeftDown = KeyEvent.VK_Z, shipRightUp = KeyEvent.VK_CLOSE_BRACKET,
+			shipRightLaser = KeyEvent.VK_QUOTE, shipRightDown = KeyEvent.VK_SLASH;
+	
 	private class GamePanel extends JPanel {
+		
+		public GamePanel() {
+			addKeyListener(new KeyListener() {
+				
+				@Override
+				public void keyTyped(KeyEvent arg0) {
+					//Does nothing
+				}
+				
+				@Override
+				public void keyReleased(KeyEvent arg0) {
+					synchronized (keysPressed) {
+						synchronized (keysReleased) {
+							keysReleased.add(arg0.getKeyCode());
+						}
+					}
+				}
+				
+				@Override
+				public void keyPressed(KeyEvent arg0) {
+					int keyCode = arg0.getKeyCode();
+					synchronized (keysPressed) {
+						synchronized (keysReleased) {
+							keysPressed.add(keyCode);
+							keysReleased.remove(keyCode);
+						}
+					}
+				}
+			});
+		}
+		
 		@Override
 		public void paintComponents(Graphics g) {
 			// TODO Auto-generated method stub
