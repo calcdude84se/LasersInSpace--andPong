67ff360f0913299bd358a1e1ae8c066801927e34
Brandon Davis <bdavis@redspin.net>
2014-03-01 14:25:38 -0500

    Added Asteroid

---
+++ src/lisp/Asteroid.java
@@ -0,0 +1,27 @@
+package lisp;
+import java.awt.Graphics2D;
+
+public class Asteroid implements GameObject{
+	
+	private int x;
+	private int y;
+	private int r;
+	
+	public Asteroid(int x, int y, int r){
+		this.x = x;
+		this.y = y;
+		this.r = r;
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		g.drawOval(x, y, r, r);
+	}
+
+	@Override
+	public void step() {
+		throw new RuntimeException("What the fuck are you doing stepping an Asteroid??");
+		
+	}
+	
+}
