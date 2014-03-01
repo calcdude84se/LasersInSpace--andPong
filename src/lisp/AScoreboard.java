4d10f7455f1cdb6b642c8782a41993ba95b0d851
ellenms <shibley@live.unc.edu>
2014-03-01 14:47:19 -0500

    added scoreboard

---
+++ src/lisp/AScoreboard.java
@@ -0,0 +1,33 @@
+package lisp;
+
+import java.awt.Graphics2D;
+
+public class AScoreboard implements GameObject{
+	
+	private static int INDENT;
+	
+	public AScoreboard(){
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		g.fillRect(0, 0, width, height);
+		drawScore(g, p1Score);
+		drawScore(g, p2Score);
+	}
+
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void drawScore(Graphics2D g, Score score) {
+		g.fillRect(INDENT, INDENT, width/2-INDENT, height/2-INDENT);
+	}
+	
+}
+	
+	
+}
