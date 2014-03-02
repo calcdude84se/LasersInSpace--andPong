00a002476de8d0df0fa4d15ff3a05d3e41b3c559
calcdude84se <calcdude84se@gmail.com>
2014-03-02 03:29:17 -0500

    Load image only once per drawer

--- src/lisp/drawers/AsteroidImageDrawer.java
+++ src/lisp/drawers/AsteroidImageDrawer.java
@@ -10,14 +10,17 @@ import javax.imageio.ImageIO;
 import lisp.AsteroidImp;
 
 public class AsteroidImageDrawer implements GameObjectDrawer<AsteroidImp> {
-
-	@Override
-	public void draw(AsteroidImp asteroid, Graphics2D g) {
-		String path = "images/asteroid.png";
-		BufferedImage image = null;
+	
+	String path = "images/asteroid.png";
+	BufferedImage image = null;
+	{
         try {image = ImageIO.read(new File(path));
         } catch (IOException e){
         }
+	}
+
+	@Override
+	public void draw(AsteroidImp asteroid, Graphics2D g) {
         g.drawImage(image,
         		(int)asteroid.x,(int)asteroid.y,(int)(2*asteroid.r),(int)(2*asteroid.r),
         		null);
