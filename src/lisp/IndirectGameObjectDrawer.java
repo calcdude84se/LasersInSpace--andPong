102686770f299ddd684aec43d07673374bad675a
calcdude84se <calcdude84se@gmail.com>
2014-03-02 02:51:07 -0500

    created IndirectGameObjectDrawer

---
+++ src/lisp/IndirectGameObjectDrawer.java
@@ -0,0 +1,27 @@
+package lisp;
+
+import java.awt.Graphics2D;
+
+import lisp.drawers.GameObjectDrawer;
+
+public class IndirectGameObjectDrawer<T extends GameObject> implements GameObjectDrawer<T> {
+
+	private GameObjectDrawer<T> drawer;
+	
+	public IndirectGameObjectDrawer(GameObjectDrawer<T> drawer) {
+		this.drawer = drawer;
+	}
+
+	public GameObjectDrawer<T> getDrawer() {
+		return drawer;
+	}
+
+	public void setDrawer(GameObjectDrawer<T> drawer) {
+		this.drawer = drawer;
+	}
+
+	@Override
+	public void draw(T go, Graphics2D g) {
+		drawer.draw(go, g);
+	}
+}
