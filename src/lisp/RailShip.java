6650bda786820176f09f6db2f06eb3af237887b9
hastings greer <hastings.greer@gmail.com>
2014-03-01 21:15:36 -0500

    iAdded derpy engines

--- src/lisp/RailShip.java
+++ src/lisp/RailShip.java
@@ -4,6 +4,8 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
+import java.awt.geom.Ellipse2D;
+import java.util.Random;
 
 public class RailShip extends GameObjectInRoom implements GameObject {
 	private double x;
@@ -11,7 +13,8 @@ public class RailShip extends GameObjectInRoom implements GameObject {
 	private double y_speed = 0;
 	private boolean facesRight;
 	private int lazerCoolDown=0;
-	
+	private int thrustingUp = 0;
+	private int thrustingDown = 0;
 	
 	private GameRoom room;
 	private static final Stroke STROKE = new BasicStroke(2);
@@ -19,12 +22,16 @@ public class RailShip extends GameObjectInRoom implements GameObject {
 	public static final int SHIP_WIDTH = 20;
 	private static final int SHIP_HEIGHT = 40;
 	
+	Random r;
+	
 	public RailShip(double x, double y, boolean facesRight, GameRoom room){
 		super(room);
 		this.x = x;
 		this.y = y;
 		this.facesRight = facesRight;
 		this.room = room;
+		
+		r = new Random();
 		
 	}
 	
@@ -38,21 +45,51 @@ public class RailShip extends GameObjectInRoom implements GameObject {
 		
 		y += y_speed;
 		lazerCoolDown--;
+		thrustingUp--;
+		thrustingDown--;
 		//fireLaser();
 	}
 	
 	public void draw(Graphics2D g){
-		g.setColor(Color.GREEN);
+		
 		g.setStroke(STROKE);
+		
+		
+		//Engines
+		
+		g.setColor(Color.RED);
+		if(thrustingDown>0){
+			for(int i = 0; i<12; i++){
+				g.drawLine((int) Math.round(x+SHIP_WIDTH/2), 
+						   (int) Math.round(y), 
+						   (int) Math.round(x+SHIP_WIDTH*r.nextDouble()), 
+						   (int) Math.round(y - SHIP_HEIGHT*.5*r.nextDouble()));
+		
+			}
+		}
+		
+		if(thrustingUp>0){
+			for(int i = 0; i<12; i++){
+				g.drawLine((int) Math.round(x+SHIP_WIDTH/2), 
+						   (int) Math.round(y+SHIP_HEIGHT), 
+						   (int) Math.round(x+SHIP_WIDTH*r.nextDouble()), 
+						   (int) Math.round(y + SHIP_HEIGHT*(1+.5*r.nextDouble())));
+		
+			}
+		}
+		
+		g.setColor(Color.GREEN);
 		g.drawRect((int) Math.round(x), (int) Math.round(y), SHIP_WIDTH, SHIP_HEIGHT);
 	}
 	
 	public void accUp(){
 		y_speed-= PER_STEP_ACC;
+		thrustingUp = 3;
 	}
 	
 	public void accDown(){
 		y_speed+= PER_STEP_ACC;
+		thrustingDown = 3;
 	}
 	
 	public void fireLaser(){
@@ -72,5 +109,17 @@ public class RailShip extends GameObjectInRoom implements GameObject {
 		room.getScoreBoard().setScore(facesRight?1:0);
 		return false;
 		
+	}
+	
+	public void collideAsteroids(){
+		for(AsteroidImp ast : CollectionUtilities.cloneArrayList(room.getAsteroidField().getAsteroids())){
+			Ellipse2D asteroid = new Ellipse2D.Double(ast.getXCenter() - ast.getR(), ast.getYCenter()-ast.getR(), 2*ast.getR(), 2*ast.getR());
+			if(asteroid.intersects(x, y, SHIP_WIDTH, SHIP_HEIGHT)){			
+				room.addObject(new Explosion(ast.getXCenter(), ast.getYCenter(), room, Color.BLUE));
+				room.getScoreBoard().setScore(facesRight?1:0);
+				ast.destroy();
+			}
+			
+		}
 	}
 }
