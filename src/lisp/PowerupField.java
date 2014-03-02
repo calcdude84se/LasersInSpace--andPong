bf18728836cce54f354db9617758c61401f45a15
calcdude84se <calcdude84se@gmail.com>
2014-03-02 11:53:40 -0500

    Generate Unicorns whenever there aren't any

--- src/lisp/PowerupField.java
+++ src/lisp/PowerupField.java
@@ -1,12 +1,15 @@
 package lisp;
 
 import java.awt.Graphics2D;
+import java.util.Random;
 
 import lisp.powerups.Powerup;
+import lisp.powerups.Unicorn;
 
 public class PowerupField extends GameObjectInRoom {
 	
 	private Powerup<?> powerup = null;
+	private Random r = new Random();
 	
 	public PowerupField(GameRoom room) {
 		super(room);
@@ -21,10 +24,12 @@ public class PowerupField extends GameObjectInRoom {
 
 	@Override
 	public void step() {
-		//TODO step magic
 		Powerup<?> powerup = this.powerup;
 		if(powerup != null)
 			powerup.step();
+		else
+			this.powerup = new Unicorn(room, this, r.nextDouble()*room.getPanel().getWidth(), 0,
+					r.nextDouble()*4-2, r.nextDouble()*3+1, r.nextDouble()*30+30, r.nextInt(2));
 	}
 
 	public void doPowerupAt(double x, double y) {
