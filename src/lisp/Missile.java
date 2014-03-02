feb691a6a3cc0e64dac2de47df5151cec1de5d36
hastings greer <hastings.greer@gmail.com>
2014-03-01 21:30:45 -0500

    Beginning misile development

---
+++ src/lisp/Missile.java
@@ -0,0 +1,21 @@
+package lisp;
+
+import java.awt.Graphics2D;
+
+public class Missile extends GameObjectInRoom {
+	
+	double x;
+	double y;
+	
+	Missile(GameRoom room){
+		super(room);
+	}
+	
+	public void draw(Graphics2D g){
+		
+	}
+	
+	public void step(){
+		
+	}
+}
